7f9149650defa9198ffd42b6a029460b42f830a4
mazakpe <makpalyy@gmail.com>
2018-11-08 15:23:19 +0300

    ADD: add page uder construction component

---
+++ src/app/page-under0construction/page-under0construction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-under0construction',
+  templateUrl: './page-under0construction.component.html',
+  styleUrls: ['./page-under0construction.component.css']
+})
+export class PageUnder0constructionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
